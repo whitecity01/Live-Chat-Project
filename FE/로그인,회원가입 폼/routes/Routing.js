ac472c88066364181596719ae37dcf1ea0429cde
whitecity01 <kh081104@gmail.com>
2023-08-20 14:57:42 +0900

    [edit] 쓸모 없는 코드 삭제

--- FE/로그인,회원가입 폼/routes/Routing.js
+++ FE/로그인,회원가입 폼/routes/Routing.js
@@ -1,8 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-// import Todo from '../compoent/Todo/Todo';
-// import News from '../compoent/News/News';
-// import Weather from '../compoent/Weather/Weather';
-// import Movie from '../compoent/Movie/Movie';
 import Login from '../compoent/Login/Login';
 import Register from '../compoent/Register/Register';
 
